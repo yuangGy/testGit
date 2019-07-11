2132ffecbb1a9d98212793ff7071630ead0604f8
gy_pro <gy_pro@hand.com>
2019-07-11 11:57:24 +0800

    T1000继续修改脚本

--- src/main/java/com/gy/git/Happy.java
+++ src/main/java/com/gy/git/Happy.java
@@ -3,5 +3,8 @@ package com.gy.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("sss");
+		System.out.println("这是T1000修改的一个脚本");
+		System.out.println("这是T100新增加的脚本");
+		System.out.println("T1000继续修改脚本");
 	}
 }
